770041706981c99711a1b977e84a4a37d8ccefd2
Sylvia Chin <sylviadoce@gmail.com>
2021-02-18 00:08:15 -0800

    clicks

--- BoardGUI.py
+++ BoardGUI.py
@@ -7,7 +7,7 @@
 from graphics import *
 from Button import *
 from Square import *
-from Piece import *
+##from Piece import *
 
 class BoardGUI:
     def __init__(self):
@@ -65,147 +65,202 @@ class BoardGUI:
             Text(point,num).draw(self.win)
             y += 1
 
-    def createPawns(self) -> list:
-        """Creates all white, black pawns in their standard locations."""
-        # Stores a list of two lists, pawns in each color (white first)
-        pawns = [[],[]]
-        
-        # Initialize the white (bottom-left) pawn coordinates (a2), create
-        x,y = 0.5,1.5
-        for i in range(8):
-            pawns[0] += Pawn(Point(x,y),self.win,"white","pawn")
-            x += 1
-
-        # Initialize the black (top-left) pawn coordinates (a7), create
-        x,y = 0.5,6.5
-        for i in range(8):
-            pawns[1] += Pawn(Point(x,y),self.win,"black","pawn")
-
-        return pawns
-        
-    def createBishops(self) -> list:
-        """Creates all white, black bishops in their standard locations."""
-        # Stores a list of two lists, bishops in each color (white first)
-        bishops = [[],[]]
-
-        # Initialize the white bishop coordinates (c1 and f1), create
-        bishops[0] += Bishop(Point(2.5,0.5),self.win,"white","bishop")
-        bishops[0] += Bishop(Point(5.5,0.5),self.win,"white","bishop")
-
-        # Initialize the black bishop coordinates (c8 and f8), create
-        bishops[1] += Bishop(Point(2.5,7.5),self.win,"black","bishop")
-        bishops[1] += Bishop(Point(5.5,7.5),self.win,"black","bishop")
-
-        return bishops
-
-    def createKnights(self) -> list:
-        """Creates all white, black knights in their standard locations."""
-        # Stores a list of two lists, knights in each color (white first)
-        knights = [[],[]]
-
-        # Initialize the white knight coordinates (b1 and g1), create
-        knights[0] += Knight(Point(1.5,0.5),self.win,"white","knight")
-        knights[0] += Knight(Point(6.5,0.5),self.win,"white","knight")
-
-        # Initialize the black knight coordinates (b8 and g8), create
-        knights[1] += Knight(Point(1.5,7.5),self.win,"black","knight")
-        knights[1] += Knight(Point(6.5,7.5),self.win,"black","knight")
-
-        return knights
-
-    def createRooks(self) -> list:
-        """Creates all white, black rooks in their standard locations."""
-        # Stores a list of two lists, rooks in each color (white first)
-        rooks = [[],[]]
-
-        # Initialize the white rook coordinates (a1 and h1), create
-        rooks[0] += Rook(Point(0.5,0.5),self.win,"white","rook")
-        rooks[0] += Rook(Point(7.5,0.5),self.win,"white","rook")
-
-        # Initialize the black rook coordinates (a8 and h8), create
-        rooks[1] += Rook(Point(0.5,7.5),self.win,"black","rook")
-        rooks[1] += Rook(Point(7.5,7.5),self.win,"black","rook")
-
-        return rooks
-
-    def createQueens(self) -> list:
-        """Creates all white, black queens in their standard locations."""
-        # Stores a list of two lists, queens in each color (white first)
-        queens = [[],[]]
-
-        # Initialize the white queen coordinates (d1), create
-        queens[0] += Queen(Point(3.5,0.5),self.win,"white","queen")
-
-        # Initialize the black queen coordinates (d8), create
-        queens[1] += Queen(Point(3.5,7.5),self.win,"black","queen")
-
-        return queens
-
-    def createKings(self) -> list:
-        """Creates all white, black kings in their standard locations."""
-        # Stores a list of two lists, kings in each color (white first)
-        kings = [[],[]]
-
-        # Initialize the white king coordinates (e1), create
-        kings[0] += King(Point(4.5,0.5),self.win,"white","king")
-
-        # Initialize the black king coordinates (e8), create
-        kings[1] += King(Point(4.5,7.5),self.win,"black","king")
-
-        return kings
-
-##    def createPieces(self) -> list:
-##        """Return a list of two lists of created objects: white pieces and
-##            black pieces (white is 0, black is 1)."""
+##    def createPawns(self) -> list:
+##        """Creates all white, black pawns in their standard locations."""
+##        # Stores a list of two lists, pawns in each color (white first)
+##        pawns = [[],[]]
+##        
+##        # Initialize the white (bottom-left) pawn coordinates (a2), create
+##        x,y = 0.5,1.5
+##        for i in range(8):
+##            pawns[0] += Pawn(Point(x,y),self.win,"white","pawn")
+##            x += 1
 ##
-##        # Create all pieces from their subclass
-##        pawns = self.createPawns()
-##        bishops = self.createBishops()
-##        knights = self.createKnights()
-##        rooks = self.createRooks()
-##        queens = self.createQueens()
-##        kings = self.createKings()
+##        # Initialize the black (top-left) pawn coordinates (a7), create
+##        x,y = 0.5,6.5
+##        for i in range(8):
+##            pawns[1] += Pawn(Point(x,y),self.win,"black","pawn")
 ##
-##        # Create a list of two lists storing all pieces
-##        # First item contains white pieces, second contains black pieces
-##        pieces = [[],[]]
-##        for color in range(2):
-##            for piece in [pawns,bishops,knights,rooks,queens,kings]:
-##                pieces[color] += piece[color]
+##        return pawns
+##        
+##    def createBishops(self) -> list:
+##        """Creates all white, black bishops in their standard locations."""
+##        # Stores a list of two lists, bishops in each color (white first)
+##        bishops = [[],[]]
 ##
-####        # Draw all pieces in the graphics window
+##        # Initialize the white bishop coordinates (c1 and f1), create
+##        bishops[0] += Bishop(Point(2.5,0.5),self.win,"white","bishop")
+##        bishops[0] += Bishop(Point(5.5,0.5),self.win,"white","bishop")
+##
+##        # Initialize the black bishop coordinates (c8 and f8), create
+##        bishops[1] += Bishop(Point(2.5,7.5),self.win,"black","bishop")
+##        bishops[1] += Bishop(Point(5.5,7.5),self.win,"black","bishop")
+##
+##        return bishops
+##
+##    def createKnights(self) -> list:
+##        """Creates all white, black knights in their standard locations."""
+##        # Stores a list of two lists, knights in each color (white first)
+##        knights = [[],[]]
+##
+##        # Initialize the white knight coordinates (b1 and g1), create
+##        knights[0] += Knight(Point(1.5,0.5),self.win,"white","knight")
+##        knights[0] += Knight(Point(6.5,0.5),self.win,"white","knight")
+##
+##        # Initialize the black knight coordinates (b8 and g8), create
+##        knights[1] += Knight(Point(1.5,7.5),self.win,"black","knight")
+##        knights[1] += Knight(Point(6.5,7.5),self.win,"black","knight")
+##
+##        return knights
+##
+##    def createRooks(self) -> list:
+##        """Creates all white, black rooks in their standard locations."""
+##        # Stores a list of two lists, rooks in each color (white first)
+##        rooks = [[],[]]
+##
+##        # Initialize the white rook coordinates (a1 and h1), create
+##        rooks[0] += Rook(Point(0.5,0.5),self.win,"white","rook")
+##        rooks[0] += Rook(Point(7.5,0.5),self.win,"white","rook")
+##
+##        # Initialize the black rook coordinates (a8 and h8), create
+##        rooks[1] += Rook(Point(0.5,7.5),self.win,"black","rook")
+##        rooks[1] += Rook(Point(7.5,7.5),self.win,"black","rook")
+##
+##        return rooks
+##
+##    def createQueens(self) -> list:
+##        """Creates all white, black queens in their standard locations."""
+##        # Stores a list of two lists, queens in each color (white first)
+##        queens = [[],[]]
+##
+##        # Initialize the white queen coordinates (d1), create
+##        queens[0] += Queen(Point(3.5,0.5),self.win,"white","queen")
+##
+##        # Initialize the black queen coordinates (d8), create
+##        queens[1] += Queen(Point(3.5,7.5),self.win,"black","queen")
+##
+##        return queens
+##
+##    def createKings(self) -> list:
+##        """Creates all white, black kings in their standard locations."""
+##        # Stores a list of two lists, kings in each color (white first)
+##        kings = [[],[]]
+##
+##        # Initialize the white king coordinates (e1), create
+##        kings[0] += King(Point(4.5,0.5),self.win,"white","king")
+##
+##        # Initialize the black king coordinates (e8), create
+##        kings[1] += King(Point(4.5,7.5),self.win,"black","king")
+##
+##        return kings
+##
+####    def createPieces(self) -> list:
+####        """Return a list of two lists of created objects: white pieces and
+####            black pieces (white is 0, black is 1)."""
+####
+####        # Create all pieces from their subclass
+####        pawns = self.createPawns()
+####        bishops = self.createBishops()
+####        knights = self.createKnights()
+####        rooks = self.createRooks()
+####        queens = self.createQueens()
+####        kings = self.createKings()
+####
+####        # Create a list of two lists storing all pieces
+####        # First item contains white pieces, second contains black pieces
+####        pieces = [[],[]]
 ####        for color in range(2):
-####            for piece in pieces[color]:
-####                piece.draw(self.win)
+####            for piece in [pawns,bishops,knights,rooks,queens,kings]:
+####                pieces[color] += piece[color]
+####
+######        # Draw all pieces in the graphics window
+######        for color in range(2):
+######            for piece in pieces[color]:
+######                piece.draw(self.win)
+####
+####        return pieces
 ##
-##        return pieces
+##    def locationCoordToLabel(self) -> str:
+##        """Converts a piece's coordinate to its alphabetized and
+##            numeric label."""
+##        
+##        loc_label = ""
+##        coord = 0.5
+##        # Associate the y/x coordinate with its respective letter/number
+##        for y in range(8):
+##            if self.piece[1][1] == coord:
+##                loc_label += str(self.board_label[0][y])
+##                break
+##            coord += 1
+##        coord = 0.5
+##        for x in range(8):
+##            if self.piece[1][0] == coord:
+##                loc_label += str(self.board_label[1][x])
+##                break
+##            coord += 1
+##
+##        return loc_label
+##
 
-    def locationCoordToLabel(self) -> str:
-        """Converts a piece's coordinate to its alphabetized and
-            numeric label."""
+    def updateMessage(self,update):
+        """Sets the message to update."""
+        self.message.setText(update)
+
+    def checkBoardStatus(self) -> int:
+        """Returns the number of active board squares."""
+        active = 0
+        for sq in self.squares:
+            if sq.active == True:
+                active += 1
+
+        return active
+
+    def allClicks(self):
+        """Handles all mouse clicks and carries out its respective
+            action and/or message update. Returns the clicked item."""
+        # Detect user mouse clicks
+        pt = self.win.getMouse()
         
-        loc_label = ""
-        coord = 0.5
-        # Associate the y/x coordinate with its respective letter/number
-        for y in range(8):
-            if self.piece[1][1] == coord:
-                loc_label += str(self.board_label[0][y])
-                break
-            coord += 1
-        coord = 0.5
-        for x in range(8):
-            if self.piece[1][0] == coord:
-                loc_label += str(self.board_label[1][x])
-                break
-            coord += 1
+        # If the quit button is clicked, close the window
+        if self.quit_button.clicked(pt) == True:
+            self.win.close()
+            
+        # Check if a board square is clicked
+        for sq in self.squares:
+            # Identify the clicked square
+            if sq.clicked(pt) == True:
+                # If another square is activated, check valid move
+                if self.checkBoardStatus != 0:
+                    return "move sq"
+                else:
+                    return "wrong sq"
+                # If empty, update the message
+                if sq.occupied == False:
+                    # UPDATE THE MESSAGES IN CHESSGAME TO GET CORRECT TEAM
+                    return "empty sq"
+                else:
+                    # check if the square's coords match a piece's coords
+                    for piece in pieces:
+                        for p in piece:
+                            if sq.getLocation() == p.getLocation():
+                                if p.checkColor == #team turn's color:
+                                    # activate the possible squares
+                                    # deactivate all other squares
+                                    return "good sq"
+                                else:
+                                    return "wrong sq"
 
-        return loc_label
-
+        return "quit"
 
 ###### END OF CLASS ######
 def main():
     board = BoardGUI()
+    board.allClicks()
 
 #TO-DOs:
+#   1. Keep track of which color team's turn it is
+#   2. Figure out what piece color is on the square
 
 #QUESTIONS:
+#   1. Is this truly encapsulated if I need Square and Piece to make it work?
+#   2. What's the best way to track which team's turn it is?
