e0ab437fc68ca8b2c44daf30991d8365939ed1bd
sahilagr456 <74263924+sahilagr456@users.noreply.github.com>
2021-02-25 09:41:13 -0800

    Add files via upload

--- BishopKingQueen_Sahil.py
+++ BishopKingQueen_Sahil.py
@@ -39,7 +39,7 @@ class King(Piece):
         numSpaces = 1
         listDir = [[0,1],[1,0],[0,-1],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]]
 
-        return listDir
+        return listDir,1
 
     def getNumSpaces(self):
         return 1
@@ -55,7 +55,7 @@ class Bishop(Piece):
         numSpaces = 10
         listDir = [[1,1],[1,-1],[-1,1],[-1,-1]]
 
-        return listDir
+        return listDir,10
 
     def getNumSpaces(self):
         return 10
